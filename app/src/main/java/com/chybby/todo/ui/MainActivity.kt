7c2d89c2440e44b1faaeba5e7af429e1bb64f825
Adam Chyb <achybby@gmail.com>
2026-07-08 22:24:51 +1000

    Upgrade to Android 17

--- app/src/main/java/com/chybby/todo/ui/MainActivity.kt
+++ app/src/main/java/com/chybby/todo/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.chybby.todo.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -15,6 +16,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             TodoTheme {
                 // Sets the background of the keyboard while it's opening or closing.
