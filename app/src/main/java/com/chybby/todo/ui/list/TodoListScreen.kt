f944a1943cbcb00796a417537c2ac1d7526fad3c
Adam Chyb <achybby@gmail.com>
2026-07-08 23:47:41 +1000

    Fix keyboard hiding items

--- app/src/main/java/com/chybby/todo/ui/list/TodoListScreen.kt
+++ app/src/main/java/com/chybby/todo/ui/list/TodoListScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
@@ -254,7 +255,7 @@ fun TodoListScreen(
                 onNavigateBack = onNavigateBack,
             )
         },
-        modifier = modifier
+        modifier = modifier.imePadding()
     ) { contentPadding ->
         TodoListScreenContent(
             uiState = uiState,
